96cae3b3c3408ae6059fe93d597c58508c1f9b63
Laaguna <73413231+Laaguna@users.noreply.github.com>
2025-04-05 20:57:04 -0500

    feat: update userController
    Add the support to fetch liked-posts, followers and following

--- VisualNetworkAPI/Controllers/UserController.cs
+++ VisualNetworkAPI/Controllers/UserController.cs
@@ -74,6 +74,152 @@ namespace VisualNetworkAPI.Controllers
       return Ok(new { data = publicUser });
     }
 
+    [HttpGet]
+    [Route("users/{id}/followers")]
+    public async Task<IActionResult> GetUserFollowers(int id)
+    {
+      var userExists = await _context.Users.AnyAsync(u => u.Id == id);
+      if (!userExists)
+      {
+        return NotFound(new { message = "Usuario no encontrado" });
+      }
+
+      var followerIds = await _context.Followers
+          .Where(f => f.FollowedId == id)
+          .Select(f => f.FollowerId)
+          .ToListAsync();
+
+      var followersData = new List<object>();
+      foreach (var followerId in followerIds)
+      {
+        var follower = await _context.Users.FindAsync(followerId);
+        if (follower != null)
+        {
+          followersData.Add(new
+          {
+            Follower = new UserPublicDto
+            {
+              Id = follower.Id,
+              User = follower.User1,
+              Email = follower.Email,
+              FirstName = follower.FirstName,
+              LastName = follower.LastName,
+              DateBirth = follower.DateBirth,
+              Active = follower.Active,
+              Phone = follower.Phone,
+              Address = follower.Address,
+              Genre = follower.Genre,
+              CreatedBy = follower.CreatedBy,
+              CreatedDate = follower.CreatedDate,
+              LastUpdate = follower.LastUpdate
+            },
+            FollowDate = await _context.Followers
+                  .Where(f => f.FollowerId == followerId && f.FollowedId == id)
+                  .Select(f => f.FollowDate)
+                  .FirstOrDefaultAsync()
+          });
+        }
+      }
+
+      return Ok(new { data = followersData });
+    }
+
+    [HttpGet]
+    [Route("users/{id}/following")]
+    public async Task<IActionResult> GetUserFollowing(int id)
+    {
+      var userExists = await _context.Users.AnyAsync(u => u.Id == id);
+      if (!userExists)
+      {
+        return NotFound(new { message = "Usuario no encontrado" });
+      }
+
+      var followingIds = await _context.Followers
+          .Where(f => f.FollowerId == id)
+          .Select(f => f.FollowedId)
+          .ToListAsync();
+
+      var followingData = new List<object>();
+      foreach (var followingId in followingIds)
+      {
+        var followedUser = await _context.Users.FindAsync(followingId);
+        if (followedUser != null)
+        {
+          followingData.Add(new
+          {
+            Following = new UserPublicDto
+            {
+              Id = followedUser.Id,
+              User = followedUser.User1,
+              Email = followedUser.Email,
+              FirstName = followedUser.FirstName,
+              LastName = followedUser.LastName,
+              DateBirth = followedUser.DateBirth,
+              Active = followedUser.Active,
+              Phone = followedUser.Phone,
+              Address = followedUser.Address,
+              Genre = followedUser.Genre,
+              CreatedBy = followedUser.CreatedBy,
+              CreatedDate = followedUser.CreatedDate,
+              LastUpdate = followedUser.LastUpdate
+            },
+            FollowDate = await _context.Followers
+                  .Where(f => f.FollowerId == id && f.FollowedId == followingId)
+                  .Select(f => f.FollowDate)
+                  .FirstOrDefaultAsync()
+          });
+        }
+      }
+
+      return Ok(new { data = followingData });
+    }
+
+    [HttpGet]
+    [Route("users/{id}/liked-posts")]
+    public async Task<IActionResult> GetUserLikedPosts(int id)
+    {
+      var userExists = await _context.Users.AnyAsync(u => u.Id == id);
+      if (!userExists)
+      {
+        return NotFound(new { message = "Usuario no encontrado" });
+      }
+
+      var likedPostIds = await _context.LikePosts
+          .Where(lp => lp.UserId == id)
+          .Select(lp => lp.PostId)
+          .ToListAsync();
+
+      var likedPostsData = new List<object>();
+      foreach (var postId in likedPostIds)
+      {
+        var post = await _context.Posts.FindAsync(postId); 
+        if (post != null)
+        {
+          likedPostsData.Add(new
+          {
+            Post = new Post
+            {
+              Id = post.Id,
+              Title = post.Title,
+              Description = post.Description,
+              PostTags = post.PostTags,
+              ImageUrls = post.ImageUrls,
+              JsonPersonalizacion = post.JsonPersonalizacion,
+              CreatedBy = post.CreatedBy,
+              CreatedDate = post.CreatedDate,
+              LastUpdate = post.LastUpdate
+            },
+            LikedDate = await _context.LikePosts
+                  .Where(lp => lp.UserId == id && lp.PostId == postId)
+                  .Select(lp => lp.CreatedDate) 
+                  .FirstOrDefaultAsync()
+          });
+        }
+      }
+
+      return Ok(new { data = likedPostsData });
+    }
+
     [HttpPut]
     [Route("users/{id}")]
     public async Task<IActionResult> UpdateUser(int id, [FromBody] User user)
